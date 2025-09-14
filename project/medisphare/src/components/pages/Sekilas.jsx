16d6bd24e2cf110c792ac5d83dd98351c6b27397
Dayana-K-H <harahapdayana01@gmail.com>
2025-09-14 22:04:47 +0700

    feat: add section Sekilas

--- project/medisphare/src/components/pages/Sekilas.jsx
+++ project/medisphare/src/components/pages/Sekilas.jsx
@@ -1,32 +1,30 @@
 import React from "react";
-import dokterImg from "../../assets/medis.svg";
-import briefcaseIcon from "../../assets/icon-briefcase.svg";
-import userIcon from "../../assets/icon-user.svg";
+import medisImg from "../../assets/medis.svg";
 
 const Sekilas = () => {
   return (
     <section className="flex flex-col md:flex-row items-center justify-between gap-12 max-w-6xl mx-auto px-6 py-16">
-      {/* Left - Gambar */}
+      {/* Pict */}
       <div className="relative w-full md:w-1/2">
         <img
-          src={dokterImg}
+          src={medisImg}
           alt="Dokter Medisphere"
           className="rounded-xl shadow-lg"
         />
       </div>
 
-      {/* Right - Teks */}
+      {/* Desc Text */}
       <div className="w-full md:w-1/2">
-        <h2 className="text-4xl font-bold text-[#404040] mb-6">
+        <h2 className="text-4xl font-bold text-black mb-6">
           Sekilas Tentang Medisphere
         </h2>
-        <p className="text-gray-600 leading-relaxed mb-6">
+        <p className="text-black leading-relaxed mb-6">
           Lorem ipsum dolor sit amet consectetur adipiscing elit. Vitae
           pellentesque sem placerat in id cursus mi. Tempus leo eu aenean sed
           diam urna tempor. Nec metus bibendum egestas iaculis massa nisl
           malesuada.
         </p>
-        <button className="bg-[#ADD5EC] text-white px-6 py-2 rounded-lg font-medium hover:bg-[#7bbbdc] transition">
+        <button className="bg-blue-500 text-blue-50 px-6 py-2 rounded-lg font-medium hover:bg-blue-700 transition">
           Selengkapnya
         </button>
       </div>
