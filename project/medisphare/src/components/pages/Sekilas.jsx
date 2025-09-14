5b0c39b23b49f325f2037f20721d147ea1f6c825
Dayana-K-H <harahapdayana01@gmail.com>
2025-09-14 21:48:55 +0700

    feat: add section Sekilas

--- project/medisphare/src/components/pages/Sekilas.jsx
+++ project/medisphare/src/components/pages/Sekilas.jsx
@@ -1,4 +1,7 @@
-import React from "react"
+import React from "react";
+import dokterImg from "../../assets/medis.svg";
+import briefcaseIcon from "../../assets/icon-briefcase.svg";
+import userIcon from "../../assets/icon-user.svg";
 
 const Sekilas = () => {
   return (
@@ -6,30 +9,10 @@ const Sekilas = () => {
       {/* Left - Gambar */}
       <div className="relative w-full md:w-1/2">
         <img
-          src="/assets/dokter.png"
+          src={dokterImg}
           alt="Dokter Medisphere"
           className="rounded-xl shadow-lg"
         />
-
-        {/* Icon kiri bawah */}
-        <div className="absolute -left-6 bottom-6 bg-[#38515F] p-3 rounded-lg">
-          <img src="/assets/icon-user.svg" alt="icon user" className="w-8 h-8" />
-        </div>
-
-        {/* Icon kanan atas */}
-        <div className="absolute -right-6 -top-6 bg-[#F5DABF] p-3 rounded-lg">
-          <img src="/assets/icon-briefcase.svg" alt="icon briefcase" className="w-8 h-8" />
-        </div>
-
-        {/* Tagline */}
-        <div className="absolute left-1/2 bottom-4 -translate-x-1/2 flex gap-3">
-          <span className="bg-[#ADD5EC] text-[#38515F] font-medium px-4 py-1 rounded-full text-sm">
-            Kesehatan berkualitas
-          </span>
-          <span className="bg-[#F5DABF] text-[#404040] font-medium px-4 py-1 rounded-full text-sm">
-            Hidup berkualitas
-          </span>
-        </div>
       </div>
 
       {/* Right - Teks */}
@@ -48,7 +31,7 @@ const Sekilas = () => {
         </button>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Sekilas
+export default Sekilas;
