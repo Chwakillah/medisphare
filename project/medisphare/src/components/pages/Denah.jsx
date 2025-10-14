4542e6526cdafd14eff2d6740dbf9e8983a1ad8b
Dayana-K-H <harahapdayana01@gmail.com>
2025-10-14 23:18:23 +0700

    fix: section for profile page

--- project/medisphare/src/components/pages/Denah.jsx
+++ project/medisphare/src/components/pages/Denah.jsx
@@ -3,10 +3,10 @@ import denahImg from "../../assets/denah.svg";
 
 const Denah = () => {
   return (
-    <section className="w-full bg-blue-50 py-16">
-      <div className="max-w-6xl mx-auto flex flex-col items-center gap-10 px-6">
-        <h2 className="text-4xl font-semibold text-black">Denah</h2>
-
+    <section className="w-full min-h-screen bg-blue-50 py-16 px-4 ">
+      <div className="max-w-6xl mx-auto">
+        <h2 className="text-4xl font-bold text-center text-black mb-12">
+          Denah</h2>
         <div className="w-full">
           <img
             src={denahImg}
--- project/medisphare/src/components/pages/Profil.jsx
+++ project/medisphare/src/components/pages/Profil.jsx
@@ -1,10 +1,15 @@
+
 import React from "react";
+import Sejarah from "./Sejarah";
 import Visi from "./Visi";
 import Denah from "./Denah";
+import Navbar from "../layouts/Navbar";
 
 const Profil = () => {
   return (
     <div className="w-full min-h-screen">
+      <Navbar/>
+      <Sejarah />
       <Visi />
       <Denah />
     </div>
