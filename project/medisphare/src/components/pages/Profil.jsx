2ede778af9f8ffb82d6979d0a9fd29af6e2991c5
Chwakillah <syakillahnachwa081@gmail.com>
2025-10-14 22:39:09 +0700

    feat: navigation to sejarah

--- project/medisphare/src/components/pages/Profil.jsx
+++ project/medisphare/src/components/pages/Profil.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Sejarah from "./Sejarah";
+import Navbar from "../layouts/Navbar";
 
 const Profil = () => {
   return (
-    <div className="w-full min-h-screen">
+    <div className="w-full min-h-screen px-8 py-20 md:px-20 md:py-24 justify-center items-center bg-cover bg-center bg-fixed overflow-hidden">
+      <Navbar/>
       <Sejarah />
     </div>
   );
